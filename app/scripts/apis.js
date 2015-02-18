9e296a96e702dc93557bae716cc6e201650c7d37
jordan23jy <jordanyong23@gmail.com>
2015-02-19 01:28:06 +0800

    included foursquare api

--- app/scripts/apis.js
+++ app/scripts/apis.js
@@ -1,7 +1,7 @@
-var foursquareData = {
-	client_id : 'JZZWQ2GOQCFO4J1R03HFTPFMOTNMTQFRGV5XQ2S0KHQYPVMZ',
-	client_secret : 'HC1KWRSYXWW5TBRBPW4DKDPH4TEXEVAZQK34GXLW2X1D315G',
-	v: '20130815',
+var fsAPI = {
+	CLIENT_ID : 'JZZWQ2GOQCFO4J1R03HFTPFMOTNMTQFRGV5XQ2S0KHQYPVMZ',
+	CLIENT_SECRET : 'HC1KWRSYXWW5TBRBPW4DKDPH4TEXEVAZQK34GXLW2X1D315G',
+	V: '20130815',
 
 	getPlacesIn: function (neighborhood, successCallback, errorCallback) {
 	    var endpoint = 'https://api.foursquare.com/v2/venues/search?client_id=' + this.clientId
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,73 +3,74 @@
   * @author Jordan Yong jordanyong23@gmail.com
   */
 
+// request dataset and pass it into ViewModel if successful
+// alert is given when ajax request has failed
+$.ajax({
+  dataType: "json",
+  url: "model.json",
+  type: "GET",
+  success: function(response) {
+    model = new ViewModel(response);
+    ko.applyBindings(model);
+    //console.log(model);
+  },
+  error: function() {
+    alert('something went wrong');
+  }
+});
 
-//var Locations = function(data) {
-//  this.title = ko.observable(data.title);
-//  this.lat = ko.observable(data.lat);
-//  this.lng = ko.observable(data.lng);
-//  };
 
-$.getJSON('model.json', function(adminData) {
-  "use strict";
-  var info = adminData.data;
-  //console.log(info);
-
-var ViewModel = function() {
-  //var info;
-  //var model = $.getJSON('scripts/model.json', function(q) {
-  //  info = q.data;
-  //  return console.log(info.length);
-  //});
-
+var ViewModel = function(data) {
   var self = this;
-  //var currentLocation = null;
+
+  /** customize mapping options
+   * @copy copy datasets as values, not wrapping to observable (no change in datasets)
+   */
+  var locMapping = {
+    'copy': ['title', 'loc', 'lat', 'id']
+  }
+
+
+
+  // map model.json dataset to ViewModel
+  ko.mapping.fromJS(data, locMapping, self);
+
 
   this.markerArray = ko.observableArray([]);  // POSIBLE TO REMOVE MARKER ARRAY
-  //this.locationArray = ko.observableArray(model);
-  this.locationArray = ko.observableArray([adminData]);
-  this.location = ko.observable('');
   this.currentLocation = ko.observable('');
   this.query = ko.observable(''); // search query keyed by user
+  this.foursquare = ko.observable('');
+
   // array of markers that have been filtered with user's search query
   this.filteredMarkers = ko.computed(function() {
     return ko.utils.arrayFilter(self.markerArray(), function(marker) {
       return marker.title.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
     });
   }, this);
-  //console.log(self.filteredMarkers());
 
-  // push position data into a list of array
-  //info.forEach(function(data) {
-  //  self.locationArray.push(new Locations(data));
-  //});
+  // point loc to locations json data for easy access
+  var loc = data.locations;
 
-  // point loc to locationArray for easy access
-  var loc = info;
-  //var loc = self.filteredMarkers();
-  //console.log(loc[0].lat);
-
-  //self.filteredMarkers.subscribe(function() {
-  //  return console.log(self.filteredMarkers());
-  //})
-
-  // renders only markers that match search query
+  // render markers that match search query
   this.filteredMarkers.subscribe(function() {
+
     // clears all markers
     self.markerArray().forEach(function(marker) {
       marker.setMap(null);
     });
+
     // renders all markers that match search query
     self.filteredMarkers().forEach(function(marker) {
       marker.setMap(map);
     });
+
   });
 
   // defines map features
   var mapOptions = {
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     center: new google.maps.LatLng(38.897947, -77.036509),
-    zoom: 15,
+    zoom: 14,
     zoomControl: true,
     zoomControlOptions: {
       style: google.maps.ZoomControlStyle.SMALL,
@@ -82,20 +83,23 @@ var ViewModel = function() {
   // render map
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-  // show info window
+  // render info window
   var infowindow = new google.maps.InfoWindow();
 
-  // init: show markers and info window
+
+  // generates characteristics of each marker and stores them in an array
+  // add an event listener to set clicked marker as current location (setLocation())
   var marker, i;
   for (i = 0; i < loc.length; i++) {
     // render all markers
     marker = new google.maps.Marker({
+      // features and data sets of markers
       position: new google.maps.LatLng(loc[i].lat, loc[i].lng),
       title: loc[i].title,
       map: map,
-      animation: google.maps.Animation.DROP
+      animation: google.maps.Animation.DROP,
+      id: loc[i].id
     });
-    //console.log(google.maps);
 
     // set list of markers into an array
     self.markerArray.push(marker);
@@ -108,7 +112,7 @@ var ViewModel = function() {
     })(marker));
   }
 
-
+  // set characteristics of current marker under interaction
   this.setLocation = function(selection) {
     if (self.currentLocation() === selection ) {
       self.removeLocation();                                // if location is selected the second time in a row it is unselected
@@ -118,12 +122,13 @@ var ViewModel = function() {
       selection.setAnimation(google.maps.Animation.BOUNCE); // animate selection to bounce
       infowindow.setContent(selection.title);               // set content of selected marker's info window
       infowindow.open(map, selection);                      // render info window of selected marker
-      //map.panTo(selection.position);                        // set center of screen to selected marker
-      //console.log(selection);
+      //map.panTo(selection.position);                      // set center of screen to selected marker
+      self.getFoursquare(selection);                        // gets foursquare json data on current location
+      console.log(selection);
     }
   };
 
-
+  // remove characteristics of current marker under interaction
   this.removeLocation = function() {
     // remove details from location
     if (self.currentLocation()) {
@@ -133,18 +138,34 @@ var ViewModel = function() {
     };
   };
 
+  // request and caches foursquare data, updated into foursquare observable. on request failure, user is alerted
+  this.getFoursquare = function(selection) {
+    $.ajax({
+      dataType: "json",
+      cache: true,
+      url: "https://api.foursquare.com/v2/venues/" + selection.id + "?"
+      + "client_id=" + "JZZWQ2GOQCFO4J1R03HFTPFMOTNMTQFRGV5XQ2S0KHQYPVMZ"
+      + "&client_secret=" + "HC1KWRSYXWW5TBRBPW4DKDPH4TEXEVAZQK34GXLW2X1D315G"
+      + "&v=" + "20130815",
 
+      type: "GET",
+      success: function(q) {
+        self.foursquare(q.response.venue);
+        console.log(self.foursquare());
+        //console.log(q.response);
+      },
+      error: function() {
+        alert('unable to retrieve information');
+      }
+    });
+  };
 
 };
 
-ko.applyBindings( new ViewModel());
-
-});
 
 /****************
-
+TODO:
 responsive design
-animate bounce when clicked on list view
 auto-complete search bar
 handle errors
 ****************/
