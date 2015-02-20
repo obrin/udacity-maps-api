610e01d4fbd42ef416e11fcb6a68fceee50f9a8f
jordan23jy <jordanyong23@gmail.com>
2015-02-20 14:06:37 +0800

    commit from 27d4b6f66fe3393cf60081d9609fd5b66f508d49

--- scripts/apis.js
+++
@@ -1,1 +0,0 @@
-var foursquareData={client_id:"JZZWQ2GOQCFO4J1R03HFTPFMOTNMTQFRGV5XQ2S0KHQYPVMZ",client_secret:"HC1KWRSYXWW5TBRBPW4DKDPH4TEXEVAZQK34GXLW2X1D315G",v:"20130815",getPlacesIn:function(e,t,c){var i="https://api.foursquare.com/v2/venues/search?client_id="+this.clientId+"&client_secret="+this.clientSecret+"&ll="+e.lat+","+e.lng+"&v=20140806&m=foursquare";this.get("venues",i,t,c)}};
